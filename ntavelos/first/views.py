65acf225c41fb0ef8ad066c5fdb7f09bbd4e01df
Alexandros Ntavelos <a.ntavelos@gmail.com>
2017-08-29 23:47:45 +0000

    add personal text

--- ntavelos/first/views.py
+++ ntavelos/first/views.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from datetime import datetime
+
 from django.template import loader
 from django.http import HttpResponse
 
 
 def index(request):
+    current_year = int(datetime.now().strftime("%Y"))
+
     template = loader.get_template('first/index.html')
-    return HttpResponse(template.render({}, request))
+    return HttpResponse(
+            template.render({
+                    'brussels_years': current_year - 2013,
+                    'dev_years': current_year - 2011
+                }, request))
