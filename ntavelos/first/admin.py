86924b12e0e5c961f7cad244300e44905bd49288
Alexandros Ntavelos <a.ntavelos@gmail.com>
2017-09-02 16:02:53 +0000

    add CV model and admin

--- ntavelos/first/admin.py
+++ ntavelos/first/admin.py
@@ -3,4 +3,15 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from first.models import CV
+
+
+@admin.register(CV)
+class CVAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created',)
+    fields = ('name', 'file', 'notes', 'created',)
+    readonly_fields = ('created', 'name',)
+
+    def name(self, obj):
+        return obj.name
+    name.short_description = 'Name'
--- ntavelos/first/models.py
+++ ntavelos/first/models.py
@@ -1,6 +1,31 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import os
+import datetime
+
 from django.db import models
 
-# Create your models here.
+CV_SUBDIR = 'cv'
+
+
+def upload_cv(instance, filename):
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    fname, ext = os.path.splitext(filename)
+
+    return "{}/{}_{}{}".format(CV_SUBDIR, fname, timestamp, ext)
+
+
+class CV(models.Model):
+    file = models.FileField(upload_to=upload_cv)
+    notes = models.CharField(max_length=1024, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True, blank=True)
+
+    @property
+    def name(self):
+        return os.path.basename(self.file.path)
+
+    @property
+    def official_name(self):
+        _, ext = os.path.splitext(self.name)
+        return "Alexandros_Ntavelos_CV{}".format(ext)
